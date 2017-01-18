4f6b31a847b899a8c10fbe5afe12fd8cc5cad668
xuwei-k <6b656e6a69@gmail.com>
2017-01-18 11:56:58 +0900

    fix broken LRUCache

--- src/main/scala/lib/LRUCache.scala
+++ src/main/scala/lib/LRUCache.scala
@@ -6,7 +6,7 @@ import scala.collection.mutable
 // https://github.com/roundrop/facebook4j/blob/master/facebook4j-core/src/main/java/facebook4j/internal/util/z_F4JLRUCache.java
 case class LRUCache[K, V](maxSize: Int) {
 
-  private def cache[K, V]: mutable.Map[K, V] = new java.util.LinkedHashMap[K, V](maxSize, 0.75f, true) {
+  private[this] val cache: mutable.Map[K, V] = new java.util.LinkedHashMap[K, V](maxSize, 0.75f, true) {
     override def removeEldestEntry(eldest: java.util.Map.Entry[K, V]) = size > maxSize
   }
 
